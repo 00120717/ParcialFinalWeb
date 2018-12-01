61d1a54121f753d214322c6b5dde4959fc5a2137
Marvin Ramirez <00120717@>
2018-12-01 17:01:13 -0600

    se agrego estilo al title

---
+++ public/javascripts/main.js
@@ -0,0 +1,59 @@
+window.onload = ()=>{
+    app.init();
+}
+
+var app = {
+
+    init:function(){
+        this.AddEvents();
+        this.loadContent();
+    },
+
+    AddRow:function(data){
+        let tbody = document.getElementsByClassName('hoteles')[0],
+            tr =  document.getElementsByClassName('tr');
+            tr.innerHTML = `
+
+                <td>${data._id} <input type='submit' value='delete'><input type='submit' value='update'> </td>
+                <td>${data.nombre}<input type='submit' value='delete'><input type='submit' value='update'></td>
+                <td>${data.ubicacion}<input type='submit' value='delete'><input type='submit' value='update'></td>
+                <td>${data.precio}<input type='submit' value='delete'><input type='submit' value='update'></td>
+
+            `;
+        tbody.appendChild(tr);
+
+        tr.addEventListener("click",function(evt){
+            this.deleteHotel(evt,data,tr,tbody);
+        });
+        tr.addEventListener("click",function(evt){
+            this.updateHotel(tr,tbody,data);
+        })
+
+
+    },
+
+    updateHotel:function(evt,tr,tbody,data){
+        evt.preventDefault();
+
+        tr.innerHTML = `
+
+        <td>${data._id} <input type='submit' value='delete'><input type='submit' value='update'> </td>
+        <td>${data.nombre}<input type='submit' value='delete'><input type='submit' value='update'></td>
+        <td>${data.ubicacion}<input type='submit' value='delete'><input type='submit' value='update'></td>
+        <td>${data.precio}<input type='submit' value='delete'><input type='submit' value='update'></td>
+
+    `;
+    let form = tr.getElementsByTagName('form')[0];
+    form.innerHTML = 
+
+    let delete = this.deleteHotel;
+    let update = this.updateHotel;
+    
+
+    }
+
+    
+    
+
+
+}
